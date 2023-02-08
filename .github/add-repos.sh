a59d27e11eaf52915bfe4b051015f2100fc80486
Thomas Hoppe <thomas.hoppe@n-fuse.co>
2023-02-08 12:10:33 +0100

    fix: correct repo name

--- .github/add-repos.sh
+++ .github/add-repos.sh
@@ -2,7 +2,7 @@
 
 set -euxo pipefail
 
-helm repo add restorecommerce https://restorecommerce.github.io/helm/
+helm repo add restorecommerce https://restorecommerce.github.io/charts/
 helm repo add minio https://helm.min.io
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo add elastic https://helm.elastic.co
