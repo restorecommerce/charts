9f96b5351ec6771f4066c236607389f7b372e32b
Martin <martin.yankov@n-fuse.co>
2023-04-13 17:22:15 +0200

    replace minio with cloudserver

--- .github/add-repos.sh
+++ .github/add-repos.sh
@@ -3,7 +3,6 @@
 set -euxo pipefail
 
 helm repo add restorecommerce https://restorecommerce.github.io/charts/
-helm repo add minio https://charts.min.io/
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo add elastic https://helm.elastic.co
 helm repo add stable https://charts.helm.sh/stable
