049cffb13fba3c3d44eae2048cb86db58f619095
Vilsol <me@vil.so>
2024-03-18 17:19:20 +0200

    chore: fix directory issues

--- .github/install-chart-testing.sh
+++ .github/install-chart-testing.sh
@@ -6,5 +6,6 @@ CHART_TESTING_VERSION="3.4.0"
 
 # install chart-testing
 curl --silent --show-error --fail --location --output /tmp/chart-testing.tar.gz https://github.com/helm/chart-testing/releases/download/v"${CHART_TESTING_VERSION}"/chart-testing_"${CHART_TESTING_VERSION}"_linux_amd64.tar.gz
-tar -xf /tmp/chart-testing.tar.gz /tmp/chart-testing
+mkdir -p /tmp/chart-testing
+tar -xf /tmp/chart-testing.tar.gz -C /tmp/chart-testing
 sudo install /tmp/chart-testing/ct
--- .github/install-helm-docs.sh
+++ .github/install-helm-docs.sh
@@ -6,5 +6,6 @@ HELM_DOCS_VERSION="1.5.0"
 
 # install helm-docs
 curl --silent --show-error --fail --location --output /tmp/helm-docs.tar.gz https://github.com/norwoodj/helm-docs/releases/download/v"${HELM_DOCS_VERSION}"/helm-docs_"${HELM_DOCS_VERSION}"_Linux_x86_64.tar.gz
-tar -xf /tmp/helm-docs.tar.gz /tmp/helm-docs
-sudo install /tmp/helm-docs
+mkdir -p /tmp/helm-docs
+tar -xf /tmp/helm-docs.tar.gz -C /tmp/helm-docs
+sudo install /tmp/helm-docs/helm-docs
