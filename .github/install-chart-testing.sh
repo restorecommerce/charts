ebf9d2636fe1d48e8635ddba43c78855be603870
Vilsol <me@vil.so>
2024-03-18 17:16:13 +0200

    chore: fix env vars

--- .github/install-chart-testing.sh
+++ .github/install-chart-testing.sh
@@ -5,6 +5,6 @@ set -euxo pipefail
 CHART_TESTING_VERSION="3.4.0"
 
 # install chart-testing
-curl --silent --show-error --fail --location --output /tmp/chart-testing.tar.gz https://github.com/helm/chart-testing/releases/download/v"${HELM_DOCS_VERSION}"/chart-testing_"${HELM_DOCS_VERSION}"_linux_amd64.tar.gz
+curl --silent --show-error --fail --location --output /tmp/chart-testing.tar.gz https://github.com/helm/chart-testing/releases/download/v"${CHART_TESTING_VERSION}"/chart-testing_"${CHART_TESTING_VERSION}"_linux_amd64.tar.gz
 tar -xf /tmp/chart-testing.tar.gz /tmp/chart-testing
 sudo install /tmp/chart-testing/ct
