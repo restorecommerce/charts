cdf8503492f2acec219f0394846f30a93448f684
Vilsol <me@vil.so>
2024-03-18 17:22:51 +0200

    chore: use mv

--- .github/install-chart-testing.sh
+++ .github/install-chart-testing.sh
@@ -8,4 +8,4 @@ CHART_TESTING_VERSION="3.4.0"
 curl --silent --show-error --fail --location --output /tmp/chart-testing.tar.gz https://github.com/helm/chart-testing/releases/download/v"${CHART_TESTING_VERSION}"/chart-testing_"${CHART_TESTING_VERSION}"_linux_amd64.tar.gz
 mkdir -p /tmp/chart-testing
 tar -xf /tmp/chart-testing.tar.gz -C /tmp/chart-testing
-sudo install /tmp/chart-testing/ct
+sudo mv /tmp/chart-testing/ct /usr/local/bin/ct
