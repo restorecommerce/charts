c1c4749d31b21d37b50844cdb2a65a86707f0c25
Vilsol <me@vil.so>
2024-11-20 15:15:48 +0200

    chore: add arangodb repo

--- .github/add-repos.sh
+++ .github/add-repos.sh
@@ -6,3 +6,4 @@ helm repo add restorecommerce https://restorecommerce.github.io/charts/
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo add elastic https://helm.elastic.co
 helm repo add stable https://charts.helm.sh/stable
+helm repo add kube-arangodb https://arangodb.github.io/kube-arangodb
