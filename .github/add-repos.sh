10d0d1f31bcab20733bfe12cebf0453ec1f8f0a6
Thomas Hoppe <thomas.hoppe@n-fuse.co>
2026-06-03 10:52:11 +0200

    fix: add missing chart add

--- .github/add-repos.sh
+++ .github/add-repos.sh
@@ -4,3 +4,4 @@ set -euxo pipefail
 
 helm repo add elastic https://helm.elastic.co
 helm repo add kube-arangodb https://arangodb.github.io/kube-arangodb
+helm repo add rustfs https://charts.rustfs.com 
